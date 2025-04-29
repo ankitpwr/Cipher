6e612b97ecdc375243a10dc1a444225846aa9685
ankit pwr <ankitpam321@gmail.com>
2025-04-30 00:00:24 +0530

    update the websocket connection

--- server/index.js
+++ server/index.js
@@ -2,10 +2,17 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import http from "http";
+import { WebSocketServer } from "ws";
+import setupWebsockets from "./websocket.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+setupWebsockets(wss);
 
 const allowedOrigins = [process.env.FRONTEND_URL];
 
@@ -17,7 +24,7 @@ app.use(
 );
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 import userRouter from "./routes/userRoute.js";
 import roomRouter from "./routes/roomRoute.js";
@@ -25,4 +32,4 @@ import roomRouter from "./routes/roomRoute.js";
 app.use("/api/user", userRouter);
 app.use("/api/room", roomRouter);
 
-app.listen(PORT, "0.0.0.0");
+server.listen(PORT, "0.0.0.0");
--- server/websocket.js
+++ server/websocket.js
@@ -1,137 +1,139 @@
 import "dotenv/config";
-import { WebSocketServer } from "ws";
+// import { WebSocketServer } from "ws";
 import WebSocket from "ws";
 import url from "url";
 import redis from "./config/redisDB.js";
 import { v4 as uuidv4 } from "uuid";
-import { type } from "os";
-const PORT = process.env.PORT_WS || 8080;
-const wss = new WebSocketServer({ host: "0.0.0.0", port: PORT });
 
-const clientMap = new Map();
+// const PORT = process.env.PORT_WS || 8080;
+// const wss = new WebSocketServer({ host: "0.0.0.0", port: PORT });
 
-const joinRoom = async (roomId, subscriber, subscribedRooms) => {
-  if (!subscribedRooms.has(roomId)) {
-    await subscriber.subscribe(`room:${roomId}`); //subscribes the Redis client to the channel named "room:" + roomId
-    subscribedRooms.add(roomId);
-  }
-};
+export default function setupWebsockets(wss) {
+  const clientMap = new Map();
 
-const storeMessage = async (roomId, payload) => {
-  const timestamp = Date.now();
+  const joinRoom = async (roomId, subscriber, subscribedRooms) => {
+    if (!subscribedRooms.has(roomId)) {
+      await subscriber.subscribe(`room:${roomId}`); //subscribes the Redis client to the channel named "room:" + roomId
+      subscribedRooms.add(roomId);
+    }
+  };
 
-  const sortedSetKey = `room:${roomId}:messages`;
-  await redis.zadd(sortedSetKey, timestamp, payload);
-};
+  const storeMessage = async (roomId, payload) => {
+    const timestamp = Date.now();
 
-const handleMessage = async (
-  e,
-  userId,
-  senderSocket,
-  subscriber,
-  subscribedRooms
-) => {
-  try {
-    const parseData = JSON.parse(e.toString());
+    const sortedSetKey = `room:${roomId}:messages`;
+    await redis.zadd(sortedSetKey, timestamp, payload);
+  };
 
-    if (parseData.type === "join") {
-      const targetRoom = parseData.roomId;
-      if (targetRoom) {
-        await joinRoom(targetRoom, subscriber, subscribedRooms);
-        senderSocket.send(
-          JSON.stringify({ type: "Joined successfully", roomId: targetRoom })
-        );
+  const handleMessage = async (
+    e,
+    userId,
+    senderSocket,
+    subscriber,
+    subscribedRooms
+  ) => {
+    try {
+      const parseData = JSON.parse(e.toString());
+
+      if (parseData.type === "join") {
+        const targetRoom = parseData.roomId;
+        if (targetRoom) {
+          await joinRoom(targetRoom, subscriber, subscribedRooms);
+          senderSocket.send(
+            JSON.stringify({ type: "Joined successfully", roomId: targetRoom })
+          );
+        } else {
+          senderSocket.send(
+            JSON.stringify({
+              type: "error",
+              message: "Room is currently unavailable",
+            })
+          );
+        }
+      } else if (parseData.type === "message") {
+        const message = parseData.message;
+        const targetRoom = parseData.roomId;
+        const username = parseData.username;
+        const messageId = uuidv4();
+
+        if (!targetRoom || !message) {
+          return senderSocket.send(
+            JSON.stringify({
+              type: "error",
+              message: "room or message is not present",
+            })
+          );
+        }
+
+        const payload = JSON.stringify({
+          type: "message",
+          messageId: messageId,
+          from: userId,
+          roomId: targetRoom,
+          message: message,
+          timestamp: Date.now(),
+          username: username,
+        });
+
+        await storeMessage(targetRoom, payload);
+        await redis.publish(`room:${targetRoom}`, payload); //publishes the payload onto the Redis channel associated with the target room
+
+        setTimeout(async () => {
+          const sortedSetKey = `room:${targetRoom}:messages`;
+
+          await redis.zrem(sortedSetKey, payload);
+          const delMsg = JSON.stringify({
+            type: "delete",
+            roomId: targetRoom,
+            messageId: messageId,
+          });
+          await redis.publish(`room:${targetRoom}`, delMsg);
+        }, 60 * 1000);
       } else {
         senderSocket.send(
           JSON.stringify({
             type: "error",
-            message: "Room is currently unavailable",
+            message: "could not able to send message",
           })
         );
       }
-    } else if (parseData.type === "message") {
-      const message = parseData.message;
-      const targetRoom = parseData.roomId;
-      const username = parseData.username;
-      const messageId = uuidv4();
-
-      if (!targetRoom || !message) {
-        return senderSocket.send(
-          JSON.stringify({
-            type: "error",
-            message: "room or message is not present",
-          })
-        );
-      }
-
-      const payload = JSON.stringify({
-        type: "message",
-        messageId: messageId,
-        from: userId,
-        roomId: targetRoom,
-        message: message,
-        timestamp: Date.now(),
-        username: username,
-      });
-
-      await storeMessage(targetRoom, payload);
-      await redis.publish(`room:${targetRoom}`, payload); //publishes the payload onto the Redis channel associated with the target room
-
-      setTimeout(async () => {
-        const sortedSetKey = `room:${targetRoom}:messages`;
-
-        await redis.zrem(sortedSetKey, payload);
-        const delMsg = JSON.stringify({
-          type: "delete",
-          roomId: targetRoom,
-          messageId: messageId,
-        });
-        await redis.publish(`room:${targetRoom}`, delMsg);
-      }, 60 * 1000);
-    } else {
+    } catch (error) {
       senderSocket.send(
-        JSON.stringify({
-          type: "error",
-          message: "could not able to send message",
-        })
+        JSON.stringify({ type: "error", message: "Invalid message format" })
       );
     }
-  } catch (error) {
-    senderSocket.send(
-      JSON.stringify({ type: "error", message: "Invalid message format" })
-    );
-  }
-};
+  };
 
-wss.on("connection", (socket, req) => {
-  const { userId, username } = url.parse(req.url, true).query;
-  if (!userId) {
-    socket.close(1008, "user Id required");
-    return;
-  }
-
-  clientMap.set(userId, socket);
-
-  const subscriber = redis.duplicate(); //new Redis client connection which is then used exclusively for subscribing to channels
-  let subscribedRooms = new Set();
-
-  subscriber.on("message", (channel, message) => {
-    //means that whenever the subscriber receives a message on any subscribed channel (which corresponds to a room), it checks that the WebSocket connection is still open and then sends that message over to the client
-    if (socket.readyState == WebSocket.OPEN) {
-      socket.send(message);
+  wss.on("connection", (socket, req) => {
+    const { userId, username } = url.parse(req.url, true).query;
+    if (!userId) {
+      socket.close(1008, "user Id required");
+      return;
     }
-  });
 
-  socket.on("message", (e) => {
-    return handleMessage(e, userId, socket, subscriber, subscribedRooms);
-  });
+    clientMap.set(userId, socket);
 
-  socket.on("close", async () => {
-    for (const roomId of subscribedRooms) {
-      await subscriber.unsubscribe(`room:${roomId}`);
-    }
-    subscriber.quit();
-    clientMap.delete(userId);
-    subscribedRooms.delete;
+    const subscriber = redis.duplicate(); //new Redis client connection which is then used exclusively for subscribing to channels
+    let subscribedRooms = new Set();
+
+    subscriber.on("message", (channel, message) => {
+      //means that whenever the subscriber receives a message on any subscribed channel (which corresponds to a room), it checks that the WebSocket connection is still open and then sends that message over to the client
+      if (socket.readyState == WebSocket.OPEN) {
+        socket.send(message);
+      }
+    });
+
+    socket.on("message", (e) => {
+      return handleMessage(e, userId, socket, subscriber, subscribedRooms);
+    });
+
+    socket.on("close", async () => {
+      for (const roomId of subscribedRooms) {
+        await subscriber.unsubscribe(`room:${roomId}`);
+      }
+      subscriber.quit();
+      clientMap.delete(userId);
+      subscribedRooms.delete;
+    });
   });
-});
+}
